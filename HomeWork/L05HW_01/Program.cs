f63354a19c9c71d77e1916bd0f3c087b38b1b670
Timur <temurt_t@mail.ru>
2020-12-21 22:45:06 +0300

    initial commit for HW_05

---
+++ HomeWork/L05HW_01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+/*
+ Тимур Самигуллин
+ 1. Создать программу, которая будет проверять корректность ввода логина. Корректным логином будет строка от 2 до 10 символов, 
+содержащая только буквы латинского алфавита или цифры, при этом цифра не может быть первой:
+    а) без использования регулярных выражений;
+    б) с использованием регулярных выражений.
+ */
+namespace L05HW_01
+{
+    class Program
+    {
+        private static char[] _login = { 'a', 'b', 'c', 'd', 'i', 'f' ,'j','k','l','m','n','p','q','r','s','t','u','v','w','x','y','z', '0','1','2','3','4','5','6','7','8','9'};
+        public static char[] Login { get { return _login; } }
+        static bool IsCorrectLog(string log)
+        {
+            bool check = false;
+            char[] checkArr = log.ToCharArray();
+            for(int i = 0; i < checkArr.Length; i++)
+            {
+                for (int j = 0; j < Login.Length; j++) { 
+                    
+                }
+            }
+            return check;
+        }
+        static void LogOn()
+        {
+            Console.WriteLine("Ведите логин:");
+            string log = Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ HomeWork/L05HW_03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ Тимур Самигуллин
+    3. *Для двух строк написать метод, определяющий, является ли одна строка перестановкой другой. Регистр можно не учитывать:
+        а) с использованием методов C#;
+        б) *разработав собственный алгоритм.
+    Например:
+        badc являются перестановкой abcd.
+ */
+namespace L05HW_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

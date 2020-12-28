f669d6a3169a6cfd35d485412dd5dfa8b804cdc5
Timur <temurt_t@mail.ru>
2020-12-28 23:46:59 +0300

    homework for lesson07

---
+++ HomeWork/L07HW_01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+    Тимур Самигуллин
+    1. а) Добавить в программу «Удвоитель» подсчет количества отданных команд.
+        б) Добавить меню и команду «Играть». При нажатии появляется сообщение, 
+            какое число должен получить игрок. 
+            Игрок должен постараться получить это число за минимальное количество ходов.
+        в) * Добавить кнопку «Отменить», которая отменяет последние ходы.
+ */
+
+namespace L07HW_01
+{
+    static class  Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
